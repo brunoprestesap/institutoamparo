c630678717ec19badcfdd14ab5e353605dd4ec84
Bruno Prestes <brunoasprestes@gmail.com>
2023-09-02 13:06:16 -0300

    topo atualizado

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,15 +9,19 @@ const Hero = () => {
         alt="background hero"
       />
 
-      <div className="w-full h-screen absolute top-0 md:bottom-4 bg-white/20">
+      <div className="w-full h-screen flex items-center absolute top-0 bg-white/30">
         <div
           id="sobre"
-          className="flex flex-col text-center w-4/6 backdrop-opacity-50 rounded-2xl bg-white/50 p-5 bg-opacity-60 shadow-2xl shadow-stone-500"
+          className="container mx-auto text-center w-5/6 h-3/6 backdrop-opacity-50 rounded-2xl bg-white/50 p-5 bg-opacity-60 shadow-2xl shadow-stone-500"
         >
           <h1 className="text-4xl uppercase lg:text-4xl font-bold text-center">
             Instituto Amparo
           </h1>
-          <p>Encontre Amparo e Equilíbrio na Sua Jornada para a Saúde Mental</p>
+          <p className="text-xl">
+            Encontre Amparo e Equilíbrio na Sua Jornada para a Saúde Mental
+          </p>
+
+          <button className="bg-green-400 p-3 m-5 rounded-xl text-white">Agende sua consulta</button>
         </div>
       </div>
     </div>
